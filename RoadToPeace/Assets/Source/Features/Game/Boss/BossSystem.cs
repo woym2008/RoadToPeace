f3a10b052949e6780cd2be47cd735ff0041e86b0
woym2008 <woym-zf@hotmail.com>
2019-08-04 09:17:56 +0800

    no message

---
+++ RoadToPeace/Assets/Source/Features/Game/Boss/BossSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entitas;
+
+public class BossSystem : Feature
+{
+    public BossSystem(Contexts contexts, Services services)
+    {
+        Add(new BossInitSystem(contexts, services));
+        Add(new BossCreateSystem(contexts, services));
+        Add(new BossReadyUpdateSystem(contexts));
+        Add(new BossDebutUpdateSystem(contexts));
+        Add(new BossFightUpdateSystem(contexts));
+    }
+}
